2582a8e8cb5790ba609e1c0d61183b851eadc100
brucekimrokcmu <kwangkyk@andrew.cmu.edu>
2023-02-16 00:27:45 -0500

    changed weight from /1.2 to /1.5

--- FindPath.cpp
+++ FindPath.cpp
@@ -357,7 +357,7 @@ std::vector<std::pair<int, int>> FindPath::AStarwithMultiBackwardDijkstra(Node s
     
     // printf("goal list saved\n");
     
-    double weight = mcollisionThresh/1.2;    
+    double weight = mcollisionThresh/1.5;    
     Node* pstartNode = new Node(startNode);
     pstartNode->SetGValue(0.0);
     // printf("start setting heuristics of the startnode\n");
